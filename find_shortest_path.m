629bdf746b2c18fe601973dae1f5dfc75cbaf35e
jmason42 <jmason42@stanford.edu>
2014-04-08 16:18:27 -0700

    Placeholder for find_shortest_path

---
+++ find_shortest_path.m
@@ -0,0 +1,11 @@
+function [path_indexes, path_length] = find_shortest_path(points_x, points_y, n_iterations)
+% Attempts to find the shortest path between a set of points (see the
+% "traveling salesman" problem)
+
+% TODO: monte-carlo solver
+
+% TODO: implement approximate solver (choose first shortest path, then
+% next...)
+
+
+end
