0a1c66d3baa1d39b1811e7993112e3ac76786ad4
jmason42 <jmason42@stanford.edu>
2014-04-08 16:26:17 -0700

    Remove redundant calculations

--- distances_between_points.m
+++ distances_between_points.m
@@ -7,12 +7,14 @@ n_points = length(points_x);
 distances = zeros(n_points);
 
 for p1 = 1:n_points
-    for p2 = 1:n_points
+    for p2 = p1+1:n_points
         distances(p1, p2) = sqrt( ...
             (points_x(p1) - points_x(p2))^2 + ...
             (points_y(p1) - points_y(p2))^2 ...
             );
     end
 end
+
+distances = distances + distances';
 
 end
