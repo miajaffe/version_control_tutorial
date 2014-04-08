512ff08cd67cc37344d9be7178c5dfa0b25a755b
jmason42 <jmason42@stanford.edu>
2014-04-08 15:05:30 -0700

    Attractive point coloring

--- main.m
+++ main.m
@@ -11,15 +11,20 @@ points_y = rand(n_points, 1);
 n_clusters = 5;
 n_iterations = 100;
 
-[centroids_x, centroids_y, cluster_assignment] = kmeans(points_x, points_y, n_clusters, n_iterations);
+[~, ~, cluster_assignment] = kmeans(points_x, points_y, n_clusters, n_iterations);
 
 convhull_indexes = convhull(points_x, points_y);
 
-plot(points_x, points_y, '.')
+figure(1)
+clf
 
 hold on
 
+% adapted from http://www.mathworks.com/matlabcentral/newsreader/view_thread/287417
+
+scatter(points_x, points_y, 10, cluster_assignment, 'filled')
+
+
 plot(points_x(convhull_indexes), points_y(convhull_indexes))
-plot(centroids_x, centroids_y, 'r.')
 
 hold off
