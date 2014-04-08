5f6d440e1d357a14cb87cd0ba5d6597136e87083
jmason42 <jmason42@stanford.edu>
2014-04-08 14:16:55 -0700

    Generate and plot random points

---
+++ main.m
@@ -0,0 +1,12 @@
+% Main execution file
+
+% For the moment, all of the code will be in this file
+
+n_points = 100;
+
+% TODO: load data from a file
+points_x = rand(n_points, 1);
+points_y = rand(n_points, 1);
+
+% just plot for now
+plot(points_x, points_y)
