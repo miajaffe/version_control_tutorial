dd065e95257fb505b6898cccc708c56bdb3615a4
Emily Alsentzer <ema2016@stanford.edu>
2014-04-18 13:51:40 -0700

    Tutorial Random Change
    Added one line for tutorial testing

--- test_k_means.m
+++ test_k_means.m
@@ -4,6 +4,7 @@ points_x = [0 0.1 -0.1 1 1.1 0.9];
 points_y = [0 0.1 -0.1 1 1.1 0.9];
 n_clusters = 2;
 max_iterations = 100;
+tutorial_Var = 1;
 
 [centroids_x, centroids_y, cluster_assignment] = kmeans(points_x, points_y, ...
     n_clusters, max_iterations);
